abba86d7e474a1d0b3f82c0b1c5f7eecc52b0c62
Olly Monger <olly@yllo.co.uk>
2019-07-23 16:58:49 +0100

    Implementing onJoin to show loginmessages

--- src/main/java/com/ollymonger/loginmessage/LoginMessage.java
+++ src/main/java/com/ollymonger/loginmessage/LoginMessage.java
@@ -7,6 +7,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,7 +15,7 @@ import java.io.*;
 import java.util.List;
 import java.util.Random;
 
-public final class LoginMessage extends JavaPlugin {
+public final class LoginMessage extends JavaPlugin implements Listener {
 
     public String prefix = ChatColor.translateAlternateColorCodes('&', "&6[&7LMSG&6]&r ");
 
@@ -50,6 +51,8 @@ public final class LoginMessage extends JavaPlugin {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -83,9 +86,10 @@ public final class LoginMessage extends JavaPlugin {
                     sender.sendMessage(prefix + "Instead of spaces, use _ to create spaces between words, corrected on join.");
                     sender.sendMessage(prefix + "/loginmessage help, /loginmessage <message>, /lm <message>.");
                 }
-                if (args.length >= 1){
-                    sender.sendMessage(prefix + "Returning the LoginMessage: " + args);
+                if (args.length == 1 && args[0].equalsIgnoreCase(args[0])){
+                    sender.sendMessage(prefix + "Returning the LoginMessage: " + args[0]);
                     List<String> list = this.getConfig().getStringList("loginmessages");
+                    list.add(args[0].replace("_", " "));
                     this.getConfig().set("loginmessages", list);
                     saveConfig();
                     getLogger().info("Saved.");
@@ -102,9 +106,10 @@ public final class LoginMessage extends JavaPlugin {
                     sender.sendMessage(prefix + "Instead of spaces, use _ to create spaces between words, corrected on join.");
                     sender.sendMessage(prefix + "/loginmessage help, /loginmessage <message>, /lm <message>.");
                 }
-                if (args.length >= 1){
-                    sender.sendMessage(prefix + "Returning the LoginMessage: " + args);
+                if (args.length == 1 && args[0].equalsIgnoreCase(args[0])){
+                    sender.sendMessage(prefix + "Returning the LoginMessage: " + args[0]);
                     List<String> list = this.getConfig().getStringList("loginmessages");
+                    list.add(args[0].replace("_", " "));
                     this.getConfig().set("loginmessages", list);
                     saveConfig();
                     getLogger().info("Saved.");
@@ -114,8 +119,4 @@ public final class LoginMessage extends JavaPlugin {
         return true;
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
 }
