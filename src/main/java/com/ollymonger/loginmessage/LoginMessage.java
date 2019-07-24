a3ba357eca7e5ab9ef944efd7c7b285cece09fa9
Olly Monger <olly@yllo.co.uk>
2019-07-24 15:26:20 +0100

    Checks if config is empty and then writes a default

--- src/main/java/com/ollymonger/loginmessage/LoginMessage.java
+++ src/main/java/com/ollymonger/loginmessage/LoginMessage.java
@@ -1,31 +1,37 @@
 package com.ollymonger.loginmessage;
 
-import jdk.internal.util.xml.impl.Input;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.*;
+import java.util.List;
+import java.util.Random;
 
-public final class LoginMessage extends JavaPlugin {
+public final class LoginMessage extends JavaPlugin implements Listener {
 
     public String prefix = ChatColor.translateAlternateColorCodes('&', "&6[&7LMSG&6]&r ");
 
     public void getData() {
         try {
-            if(!getDataFolder().exists()){
+            if (!getDataFolder().exists()) {
                 getDataFolder().mkdirs(); //makes directory if none exists
             }
             File file = new File(getDataFolder(), "config.yml"); //sets file name and file folder
-            if(!file.exists()){ //makes file if none exists
+            if (!file.exists()) { //makes file if none exists
                 getLogger().info("CONFIG NOT FOUND, CREATING A NEW CONFIG!");
-                getConfig().options().copyDefaults(false);
+                getConfig().options().copyDefaults(true);
                 saveDefaultConfig(); //saves default config
             } else {
                 getLogger().info("CONFIG FOUND! Loading!");
-                try{
+                try {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
                     this.getConfig().load(reader);
                 } catch (IOException e) {
@@ -33,18 +39,27 @@ public final class LoginMessage extends JavaPlugin {
                 } catch (InvalidConfigurationException e) {
                     e.printStackTrace();
                 }
-                if(!this.getConfig().contains("joinmessages")){
-                    this.getConfig().createSection("joinmessages");
+                if (!this.getConfig().contains("loginmessages")) {
+                    this.getConfig().createSection("loginmessages");
+                    this.getConfig().createSection("prefix");
                     getLogger().info("Config successfully set up!");
                     saveConfig();
                 }
-                if(this.getConfig() == null){
+                if (this.getConfig().getConfigurationSection("loginmessages").getKeys(false).isEmpty()) { //CHECKS IF EMPTY
+                    List<String> prefix = this.getConfig().getStringList("prefix");
+                    prefix.add("SERVER");
+                    this.getConfig().set("prefix", prefix);
+                    List<String> loginmessage = this.getConfig().getStringList("loginmessage");
+                    loginmessage.add("Welcome to the server, %player%.");
+                    this.getConfig().set("prefix", loginmessage);
                     return;
                 }
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
@@ -52,12 +67,79 @@ public final class LoginMessage extends JavaPlugin {
         getLogger().info("Plugin: LoginMessage is now enabled");
         getLogger().info("Plugin Version: " + getDescription().getVersion());
         getLogger().info("Last Updated: (23/07/19)");
-        getData(); //calls getData
+        getData();
     }
 
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) { //when a player joins run this
+        String random = this.getConfig().getStringList("loginmessages").get(new Random().nextInt(this.getConfig().getStringList("loginmessages").size())); //random selector of config.yml
+        String joinPrefix = this.getConfig().get("prefix").toString();
+        for (Player player : Bukkit.getServer().getOnlinePlayers()) {
+            String export = random.replaceAll("%player%", player.getName()); //An export of the random that looks for %player% and replaces with the player's name.
+            event.setJoinMessage(joinPrefix + " " + export); // sets the join message to the exported random.
+        }
     }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (sender.isOp()) {
+            if (cmd.getName().equalsIgnoreCase("loginmessage")) {
+                if (args.length == 0) {
+                    sender.sendMessage(prefix + "This plugin allows you to set custom (randomised) login messages.");
+                    sender.sendMessage(prefix + "Instead of spaces, use _ to create spaces between words, corrected on join.");
+                    sender.sendMessage(prefix + "/loginmessage help, /loginmessage <message>, /lm <message>.");
+                }
+                if (args.length == 1 && args[0].equalsIgnoreCase("help")){
+                    sender.sendMessage(prefix + "This plugin allows you to set custom (randomised) login messages.");
+                    sender.sendMessage(prefix + "Instead of spaces, use _ to create spaces between words, corrected on join.");
+                    sender.sendMessage(prefix + "/loginmessage help, /loginmessage <message>, /lm <message>.");
+                }
+                if (args.length == 2 && args[0].equalsIgnoreCase("prefix")){
+                    sender.sendMessage(prefix + "Returning the LoginMessage: " + args[1]);
+                    List<String> joinPrefix = this.getConfig().getStringList("prefix");
+                    joinPrefix.clear();
+                    joinPrefix.add(args[1]);
+                    this.getConfig().set("prefix", joinPrefix);
+                    saveConfig();
+                }
+                if (args.length == 1 && args[0].equalsIgnoreCase(args[0])){
+                    sender.sendMessage(prefix + "Returning the LoginMessage: " + args[0]);
+                    List<String> list = this.getConfig().getStringList("loginmessages");
+                    list.add(args[0].replace("_", " "));
+                    this.getConfig().set("loginmessages", list);
+                    saveConfig();
+                    getLogger().info("Saved.");
+                }
+            }
+            if (cmd.getName().equalsIgnoreCase("lm")) {
+                if (args.length == 0) {
+                    sender.sendMessage(prefix + "This plugin allows you to set custom (randomised) login messages.");
+                    sender.sendMessage(prefix + "Instead of spaces, use _ to create spaces between words, corrected on join.");
+                    sender.sendMessage(prefix + "/loginmessage help, /loginmessage <message>, /lm <message>.");
+                }
+                if (args.length == 1 && args[0].equalsIgnoreCase("help")){
+                    sender.sendMessage(prefix + "This plugin allows you to set custom (randomised) login messages.");
+                    sender.sendMessage(prefix + "Instead of spaces, use _ to create spaces between words, corrected on join.");
+                    sender.sendMessage(prefix + "/loginmessage help, /loginmessage <message>, /lm <message>.");
+                }
+                if (args.length == 2 && args[0].equalsIgnoreCase("prefix")){
+                    sender.sendMessage(prefix + "Returning the LoginMessage: " + args[1]);
+                    List<String> joinPrefix = this.getConfig().getStringList("prefix");
+                    joinPrefix.clear();
+                    joinPrefix.add(args[1]);
+                    this.getConfig().set("prefix", joinPrefix);
+                    saveConfig();
+                }
+                if (args.length == 1 && args[0].equalsIgnoreCase(args[0])){
+                    sender.sendMessage(prefix + "Returning the LoginMessage: " + args[0]);
+                    List<String> list = this.getConfig().getStringList("loginmessages");
+                    list.add(args[0].replace("_", " "));
+                    this.getConfig().set("loginmessages", list);
+                    saveConfig();
+                    getLogger().info("Saved.");
+                }
+            }
+        }
+        return true;
+    }
+
 }
